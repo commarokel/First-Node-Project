3f765c76a2bb2e021e02d6aab7e627f0bbe80050
MAX HO <maxhowl86@gmail.com>
2015-11-05 17:02:55 +0800

    bug in comments feature fixed

--- public/js/post-comments.js
+++ public/js/post-comments.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
 	$('.commentButton').click(function(e) {	
 		e.preventDefault();
-		var comment = $('#comment').val();
-		var answerID = $('#answerID').val();
+		var comment;
+		var answerID;
+		comment = $(this).parent().parent().prev().children('.col-sm-12').children('.comment').val();
+		answerID = $(this).parent().parent().prev().children('.col-sm-12').children('.comment').next().val();
+		console.log('the answer ID is ' + answerID);
+		console.log('the comment is ' + comment);
 	    var data = {comment: comment, answerID: answerID};
 		$.ajax({
 			type: 'POST',
 			data: data,
 			url: 'http://localhost:3000/comment',
 			success: function(data) {
-				$('.commentArray').last().append("<p class='commentContent' style='padding-left:2em; border-style:solid;border-width:1px;border-color:#d3d3d3;'>" 
-					+ data.comment + "</p>" + "<p class='commentAuthor'>" + data.author + "</p>");
-				$('#comment').val('');
+				if($('.answerArray'+ data.answerID).children().length < 6) {
+					$('.answerArray' + data.answerID).children('.ansVoteDown').after("<div class='commentArray'></div>");
+					$('.answerArray' + data.answerID).children('.commentArray').html("<p class='commentContent' style='padding-left:2em; border-style:solid;border-width:1px;border-color:#d3d3d3;'>" 
+						+ data.comment + "</p>" + "<p class='commentAuthor'>" + data.author + "</p>");
+					$('.comment').val('');
+				}
+				else if($('.answerArray'+ data.answerID).children('.commentArray')) {
+					console.log($('.answerArray'+ data.answerID).children().length);
+					$('.answerArray' + data.answerID).children('.commentArray').last().append("<p class='commentContent' style='padding-left:2em; border-style:solid;border-width:1px;border-color:#d3d3d3;'>" 
+						+ data.comment + "</p>" + "<p class='commentAuthor'>" + data.author + "</p>");
+					$('.comment').val('');
+				}
+
 			},
 			error: function(e) {
 				console.log(e);
